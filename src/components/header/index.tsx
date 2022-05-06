f926cabb2fa72a607f4865f188adf622f9bf32c6
lazaronazareno <lazaronazareno@gmail.com>
2022-05-06 14:00:26 -0300

    first commit, header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from '../../assets/logo.png'
+import './styles.scss';
+
+export default function Header () {
+  return (
+    <header className='container-fluid header d-flex position-absolute top-0 align-items-center bg-black'>
+      <img className="img-fluid header-img" src={logo} alt="img"/>
+      <span className="fs-2 ms-auto header-text">Works</span>
+      <span className="fs-2 ms-2 header-text">Contact</span>
+    </header>
+  )
+}
