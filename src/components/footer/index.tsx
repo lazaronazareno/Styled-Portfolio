09fed7f07d0ec977edeb539789e8612113e07fd8
lazaronazareno <lazaronazareno@gmail.com>
2022-05-20 20:48:57 -0300

    useWindowsSize hook, media queries

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,10 +4,10 @@ import './styles.scss';
 export default function Footer () {
   return (
     <footer className='container-fluid footer d-flex bottom-0 align-items-center'>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lazaro-vega-sanchez" className="fs-2 ms-2 header-text">LINKEDIN</a>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/lazaronazareno" className="fs-2 ms-2 header-text">GITHUB</a>
-      <span className="fs-2 ms-2 header-text">|</span>
-      <a href="mailto:lazaronazareno@gmail.com?Subject=Contacto%20por%20portfolio" className="fs-2 ms-2 header-text">LAZARONAZARENO@GMAIL.COM</a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lazaro-vega-sanchez" className="ms-2 footer-text">LINKEDIN</a>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/lazaronazareno" className="ms-2 footer-text">GITHUB</a>
+      <span className="fs-2 ms-2">|</span>
+      <a href="mailto:lazaronazareno@gmail.com?Subject=Contacto%20por%20portfolio" className="ms-2 footer-text">LAZARONAZARENO@GMAIL.COM</a>
     </footer>
   )
 }
